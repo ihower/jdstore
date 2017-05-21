78f9e0b5df7b75592b4131e100da83ed8c9d7ff6
liulupeng <liulupeng3385@163.com>
2017-05-21 22:12:55 +0800

    sendcloud settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://fast-oasis-34622.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: "smtpcloud.sohu.com",
+    port: 25,
+    domain: "heroku.com",
+    authentication: "login",
+    enable_starttls_auto: true,
+    user_name: ENV["SEND_CLOUD_USER_NAME"],
+    password: ENV["SEND_CLOUD_USER_KEY"]
+    }
 end
