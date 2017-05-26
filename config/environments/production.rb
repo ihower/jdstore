6f2376bdc85f7a8d57fffd315d0ca80e73fafaf5
liulupeng <liulupeng3385@163.com>
2017-05-26 23:54:50 +0800

    update css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
